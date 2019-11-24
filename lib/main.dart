27da1dd54550629385f990c79537fd23df4dff39
pocari <caffelattenonsugar@gmail.com>
2019-11-24 12:22:07 +0900

    hello, world

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        title: "drawer sample",
+        home: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("drawer sample!"),
+      ),
+      body: Center(
+        child: Text("Hello, world!!"),
+      ),
+    );
   }
 }
